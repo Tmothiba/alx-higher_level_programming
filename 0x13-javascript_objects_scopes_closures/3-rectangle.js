4d382fca6030f6604701101878f31e2f3eabd6fc
Tmothiba <mothibatshwarelo99@gmail.com>
2024-06-19 20:32:44 +0000

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,20 +1,20 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-    constructor(w, h) {
-        if (w <= 0 || h <= 0) {
-            // If width or height is not positive, create an empty object
-            return; // Implicitly returns undefined, creating an empty object
-        }
-        this.width = w;
-        this.height = h;
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      // If width or height is not positive, create an empty object
+      return; // Implicitly returns undefined, creating an empty object
     }
+    this.width = w;
+    this.height = h;
+  }
 
-    print() {
-        if (!this.width || !this.height) {
-            return;
-	}
-        for (let i = 0; i < this.height; i++) {
-            console.log('X'.repeat(this.width));
-        }
+  print () {
+    if (!this.width || !this.height) {
+      return;
     }
-}
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,32 +1,34 @@
 #!/usr/bin/node
-module.exports = class Rectangle{
-	constructor (w, h) {
-		if (w <= 0 || h <= 0)
-		{
-			return;
-		}
-		this.width = w;
-		this.height = h;
-	}
-print(){
-	if (!this.width || !this.height) {
-            return;
-	}
-	for (let i = 0; i < this.height; i++) {
-            console.log('X'.repeat(this.width));
-        }
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return;
     }
-rotate() {
-        if (!this.width || !this.height) {
-            return;
-	}
-	[this.width, this.height] = [this.height, this.width];
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    if (!this.width || !this.height) {
+      return;
     }
-double() {
-        if (!this.width || !this.height) {
-            return;
-	}
-	this.width *= 2;
-        this.height *= 2;
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
-}
+  }
+
+  rotate () {
+    if (!this.width || !this.height) {
+      return;
+    }
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    if (!this.width || !this.height) {
+      return;
+    }
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
