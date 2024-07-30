874d9365fa89b627756cd7301968542689622e20
Tmothiba <mothibatshwarelo99@gmail.com>
2024-07-30 19:39:26 +0000

    Star wars movie title

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const file_sys = require('fs');
+const fs = require('fs');
 
-file_sys.readFile(process.argv[2], 'utf8', function (err, data) {
-	if (err) {
-		console.log(err);
-	} else {
-		process.stdout.write(data);
-	}
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
 });
--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,5 +5,5 @@ const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2
 
 request(starWarsUri, function (_err, _res, body) {
   body = JSON.parse(body);
-  console.log(body.title); 
+  console.log(body.title);
 });
