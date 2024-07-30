f16e1bc3e8405d4dc7b258f71cfafbfc8bb0ab05
Tmothiba <mothibatshwarelo99@gmail.com>
2024-07-30 10:30:50 +0000

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@
 const file_sys = require('fs');
 
 file_sys.readFile(process.argv[2], 'utf8', function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    process.stdout.write(data);
-  }
+	if (err) {
+		console.log(err);
+	} else {
+		process.stdout.write(data);
+	}
 });
